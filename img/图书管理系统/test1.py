1cce61bae8e7a0d75027d94020e0977f9687b97d
hah007 <hah007@126.com>
2019-12-04 01:30:48 +0800

    图书馆程序

---
+++ img/图书管理系统/test1.py
@@ -0,0 +1,22 @@
+#父类需要继承object对象
+class A(object):
+    def __init__(self):
+        self.namea="aaa"
+ 
+    def funca(self):
+        print ("function a : %s"%self.namea)
+ 
+class B(A):
+    def __init__(self):
+        #这一行解决问题
+        super(B,self).__init__()
+        self.nameb="bbb"
+ 
+    def funcb(self):
+        print ("function b : %s"%self.nameb)
+ 
+b=B()
+print (b.nameb)
+b.funcb()
+ 
+b.funca()
